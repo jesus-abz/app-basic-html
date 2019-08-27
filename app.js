ddddddb8da14e58f3ffab90e4aa88cb53609b03d
jesus.abarron.z <jesus.abarron.z@gmail.com>
2019-08-27 12:44:49 -0700

    musica que suena

---
+++ app.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll('button');
+
+const func = ()=>{
+    alert('piano');
+}
+
+const playnote = event =>{
+    const button = event.target;
+    const note = button.dataset.note;
+    const audioId = `audio${note}`;
+    console.log(audioId);
+    const audio = document.getElementById(audioId);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+}
+
+buttons.forEach(
+    button => button.addEventListener('click', playnote)  
+);
